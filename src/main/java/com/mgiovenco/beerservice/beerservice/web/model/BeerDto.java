c59c71c730c29ce2c0eba416bd2fca3819e48a7c
mgiovenco <michael.e.giovenco@gmail.com>
2020-09-12 14:03:05 -0500

    Adding web data model

---
+++ src/main/java/com/mgiovenco/beerservice/beerservice/web/model/BeerDto.java
@@ -0,0 +1,31 @@
+package com.mgiovenco.beerservice.beerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createDate; // contains UTC offset
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/mgiovenco/beerservice/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.mgiovenco.beerservice.beerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
